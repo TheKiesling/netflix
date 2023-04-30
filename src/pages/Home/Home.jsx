589e1fd69ad8e707aa83eb4a33e18d439a5293a7
José Kiesling <84196494+TheKiesling@users.noreply.github.com>
2023-04-29 20:43:36 -0600

    initial config

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import styles from './Home.module.css'
+
+const Home = () => (
+  <body>
+    <header>
+      <nav>
+
+      </nav>
+    </header>
+  </body>
+)
+
+export default Home
