75e977638b48fe0b5bc4f93e705086e31bfb452f
José Kiesling <84196494+TheKiesling@users.noreply.github.com>
2023-04-30 23:51:09 -0600

    Delete components directory

--- components/NavElement/NavElement.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useState, useEffect} from 'react'
-import styles from './NavElement.module.css'
-
-const NavElement = ({text}) => {
-    return (
-    <span className={`${styles.text}`}>{text}</span>
-)}
-
-export default NavElement
